eadbdc017545bb89254058a92d304afc9795ff76
Topher <topherb99@gmail.com>
2019-10-18 13:38:08 +0100

    dice_app created with corresponding rspec

--- lib/dice_app.rb
+++ lib/dice_app.rb
@@ -1,2 +1,21 @@
-class Roll
+class Dice
+
+  def initialize
+    @score = []
+  end
+
+  def roll(num)
+    num.times do
+      @score << rand(1..6)
+    end
+  end
+
+  def prev_rolls
+    @score
+  end
+
+  def score
+    @score.sum
+  end
+
 end
--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -1,5 +1,22 @@
 require 'dice_app'
 
 describe Dice do
-  it 'Rolls a number between 1 and 6'
+  it 'Rolls a number between 1 and 6' do
+    expect(subject.roll(1)).to be_between(1, 6)
+  end
+
+  # it 'Allows user to roll as many dice as they want' do
+  #   expect(subject.roll(7)).to eq 35
+  # end
+
+  it 'Returns an array of scores' do
+    srand 200
+    subject.roll(3)
+    expect(subject.prev_rolls).to eq [3,2,1]
+  end
+
+  it 'Returns overall score' do
+    subject.roll(6)
+    expect(subject.score).to eq 21
+  end
 end
